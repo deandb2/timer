63857d0b69ad0654f1e68e6578409e8549dfb0a2
deandb2 <33088815+deandb2@users.noreply.github.com>
2025-08-28 16:15:49 +0200

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Timer3x10 from "./Timer3x10"; // Assicurati che il percorso sia corretto
+
+function App() {
+  return (
+    <div>
+      <Timer3x10 />
+    </div>
+  );
+}
+
+export default App;
